949202bd5ef3ff569d1571ded2584cdddc75dbfa
Miguel Carrera <miguelcarrera12@gmail.com>
2024-01-21 23:37:09 -0400

    hack_9 finalizado

--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,13 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
+    nueva_lista = []
+    i = 0
+    while i < len(result):
+        nueva_lista.append(result[i])
+        nueva_lista.append('@')
+        i += 1
     #...
-    return result  
+    return nueva_lista 
+
+print(fn_hack_9())
