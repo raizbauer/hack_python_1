a2f16da7e535e9f95fc4d62ec8dcdebd3e4dae9d
Miguel Carrera <miguelcarrera12@gmail.com>
2024-01-21 23:48:49 -0400

    hack_10 finalizado

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,18 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
+    nueva_lista = []
+    for char in result:
+        if char == "o":
+            nueva_lista.append("0")
+        elif char == "i":
+            nueva_lista.append("1")
+        elif char == "a":
+            nueva_lista.append("@")
+        else:
+            nueva_lista.append(char.upper())
+    
     #...
-    return result  
+    return nueva_lista  
+
+print(fn_hack_10())
