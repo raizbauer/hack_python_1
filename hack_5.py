b72c8e3e0d915fdc5e686c2d87b1acfaf9e90bc8
Miguel Carrera <miguelcarrera12@gmail.com>
2024-01-21 23:10:20 -0400

    hack_5 finalizado

--- hack_5.py
+++ hack_5.py
@@ -5,4 +5,6 @@ text: "fooziman" output => "f00z1m@n"
 def fn_hack_5():
     result = "fooziman"
     #...
-    return result  
+    return result.replace("o" , "0").replace("i" , "1").replace("a" , "@")
+
+print(fn_hack_5())
