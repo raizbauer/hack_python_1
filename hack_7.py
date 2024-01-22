e13fb5bf186b5b2b2f74d7d6dfa811cb56f245e7
Miguel Carrera <miguelcarrera12@gmail.com>
2024-01-21 23:24:13 -0400

    hack_7 finalizado

--- hack_7.py
+++ hack_7.py
@@ -3,6 +3,12 @@ loop: while [] output => [5,4,3,2,1,0]
 """
 
 def fn_hack_7():
+    i = 5
     result = []
     #...
+    while i >= 0:
+        result.append(i)
+        i -= 1
     return result  
+
+print(fn_hack_7())
